203201929bfa2d38fd79fa0e5fac6e471b34a5a8
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-01-16 11:13:50 +0600

    sadsad

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            This is home 
+            This is home  
+            <h1>Hello 2</h1>
         </div>
     );
 };
