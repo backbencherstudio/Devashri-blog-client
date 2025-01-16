d692ab99d36558a467c25354ac57fd935577bf6a
Saidul147 <sidul.islam.146@gmail.com>
2025-01-16 14:27:21 +0600

    nothing work

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from '../Shared/NavBar';
 
 const Layout = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet />
         </div>
     );
