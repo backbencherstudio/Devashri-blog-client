a9e71f79fe7b870a5013ff7823b1e015644fa588
Saidul147 <sidul.islam.146@gmail.com>
2025-01-20 15:07:52 +0600

    pdf file added

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useRef,useEffect,} from 'react';
+import React, { useState, useRef, useEffect, } from 'react';
 import logo from "../assets/Layer 5.png"
 import { Link, NavLink, useSearchParams } from 'react-router-dom';
 import { CiMenuFries } from "react-icons/ci";
@@ -14,7 +14,7 @@ const NavBar = () => {
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (
-                sidebarRef.current && 
+                sidebarRef.current &&
                 !sidebarRef.current.contains(event.target) &&
                 menuButtonRef.current &&
                 !menuButtonRef.current.contains(event.target)
@@ -44,9 +44,13 @@ const NavBar = () => {
                             <li><NavLink to={"/"}>HOME</NavLink></li>
                             <li><NavLink to={"/about"}>about</NavLink></li>
                             <li><NavLink to={"/playground"}>playground</NavLink></li>
-                            <li><NavLink to={"/resume"}>resume</NavLink></li>
+                             <li>
+                                <a href="https://drive.google.com/file/d/1WS7BvELMfcFdXZii-b_XoBcJmwco3Xjp/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                                    Resume
+                                </a>
+                            </li>
                         </ul>
-                        <button 
+                        <button
                             ref={menuButtonRef}
                             onClick={() => setOpen(!open)}
                             className="md:hidden block"
@@ -57,19 +61,18 @@ const NavBar = () => {
                 </div>
                 {/* Sidebar with overlay */}
                 <div className={`md:hidden fixed inset-0 bg-black/50 transition-all duration-700 ${open ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
-                    <div 
+                    <div
                         ref={sidebarRef}
-                        className={`uppercase w-[220px] min-h-screen bg-zinc-50 fixed top-0 left-0 pt-[36px] p-4 transition-transform duration-700 ease-out ${
-                            open ? 'translate-x-0' : '-translate-x-full'
-                        }`}
+                        className={`uppercase w-[220px] min-h-screen bg-zinc-50 fixed top-0 left-0 pt-[36px] p-4 transition-transform duration-700 ease-out ${open ? 'translate-x-0' : '-translate-x-full'
+                            }`}
                     >
                         <div className='flex items-center justify-between border-b border-[#DD0101] pb-4'>
                             <Link to={"/"}>
-                            <img src={logo || "/placeholder.svg"} className='w-[36px]' alt="logo" />
+                                <img src={logo || "/placeholder.svg"} className='w-[36px]' alt="logo" />
                             </Link>
                             <div>
-                                <IoMdClose 
-                                    onClick={() => setOpen(false)} 
+                                <IoMdClose
+                                    onClick={() => setOpen(false)}
                                     className='text-2xl cursor-pointer hover:text-[#DD0101] transition-all'
                                 />
                             </div>
@@ -78,7 +81,11 @@ const NavBar = () => {
                             <li><NavLink to={"/"}>HOME</NavLink></li>
                             <li><NavLink to={"/about"}>about</NavLink></li>
                             <li><NavLink to={"/playground"}>playground</NavLink></li>
-                            <li><NavLink to={"/resume"}>resume</NavLink></li>
+                            <li>
+                                <a href="https://drive.google.com/file/d/1WS7BvELMfcFdXZii-b_XoBcJmwco3Xjp/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                                    Resume
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
